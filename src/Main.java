72e19c2a0bd4dfd4379f1f00dead4b3a9e070ecc
AlexandrAnatoliev <per-1986@list.ru>
2024-12-26 22:09:52 +0300

    feat: update Expression.java class
    Добавил поля класса, конструктор, геттеры и сеттеры

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,19 @@ package src;
  * console-calculator
  *
  * @author AlexandrAnatoliev
- * @version 0.2.0 2024-12-26
+ * @version 0.3.0 2024-12-26
  */
-
 
 public class Main {
     public static void main(String[] args) {
+        Expression expr = new Expression("hello world");
+        expr.setNum1(1);
+        expr.setNum2(2);
+        expr.setOperation(Operations.PLUS);
+        expr.setResult(3);
         System.out.println("Main class run");
+        System.out.println(expr.getUserInput());
+        System.out.println(expr.getNum1() + " " + expr.getNum2() + " " + expr.getResult());
+        System.out.println(expr.getOperation());
     }
 }
