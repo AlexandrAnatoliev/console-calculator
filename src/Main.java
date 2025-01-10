118a80b1e31d350e1286a94f49715be26d7bdadd
AlexandrAnatoliev <per-1986@list.ru>
2025-01-10 15:41:43 +0300

    feat: add ThreeWords class
    it works to math expressions
    
    version 0.14.4

--- src/Main.java
+++ src/Main.java
@@ -4,43 +4,53 @@ package src;
  * console-calculator
  *
  * @author AlexandrAnatoliev
- * @version 0.14.3 2024-12-26
+ * @version 0.14.4 2024-12-26
  */
 
 public class Main {
     public static void main(String[] args) {
         UserInterface userInterface = new UserInterface();
         UserInput input = userInterface.createUserInput();
-        if(input.countWords() == 1)
-        {
+
+        if (input.countWords() == 1) {
             OneWord command = new OneWord(input);
-            System.out.println(command.getCommand());
-        } else if (input.countWords() == 2) {
-            System.out.println("two words");
-        }
-        else {
-            System.out.println("three");
-        }
-/*        Expression expression;
-        UserInterface userInterface = new UserInterface();
-        Calculator calculator = new Calculator();
 
-        do {
-            expression = userInterface.setUserInput();
-
-            if (userInterface.inputIsHelp(expression)) {
+            if (command.getCommand().contains("help")) {
                 userInterface.showHelp();
-                continue;
             }
-
-            if (userInterface.inputIsExit(expression)) {
+            if (command.getCommand().contains("exit")) {
+                System.out.println("good buy");
                 return;
             }
 
-            expression = userInterface.parseExpression(expression);
-            System.out.println(expression.getUserInput() + " = " + calculator.getResult(expression));
+        } else if (input.countWords() == 2) {
+            System.out.println("two words");
+        } else {
+            ThreeWords ex = new ThreeWords(input);
+            System.out.println(ex.getNum1());
+            System.out.println(ex.getNum2());
+            System.out.println(ex.getOperator());
         }
-        while (!userInterface.inputIsExit(expression));
-  */
+         /*        Expression expression;
+         UserInterface userInterface = new UserInterface();
+         Calculator calculator = new Calculator();
+
+         do {
+         expression = userInterface.setUserInput();
+
+         if (userInterface.inputIsHelp(expression)) {
+         userInterface.showHelp();
+         continue;
+         }
+
+         if (userInterface.inputIsExit(expression)) {
+         return;
+         }
+
+         expression = userInterface.parseExpression(expression);
+         System.out.println(expression.getUserInput() + " = " + calculator.getResult(expression));
+         }
+         while (!userInterface.inputIsExit(expression));
+         */
     }
 }
